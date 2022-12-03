5a9fb7b8624417427c82d4e1b6b7ae5c6bfd9698
Sleep-Ray <lishurui520@qq.com>
2022-12-04 02:48:09 +0800

    高通滤波

---
+++ src/03HFT_Demo.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project : FT_Pro
+@File    : 03HFT_Demo
+@IDE     : PyCharm
+@Author  : 李睡睡
+@Date    : 2022/12/4 2:32
+@Scripts :opencv 、 numpy 、 matplotlib
+@Function: 高通滤波的 cv 实现
+@Tips    :不要左顾右盼。慢慢积累，慢慢写吧。毕竟除了这样单调的努力，我什么也做不了。
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('../image/lena.png',0)
+
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+
+# 设置高通滤波，(去掉低频)
+rows,cols = img.shape
+crow,ccol = int(rows/2),int(cols/2)
+fshift[crow-30 : crow+30,ccol-30:ccol+30] = 0
+
+# 逆傅里叶变换
+ishift = np.fft.ifftshift(fshift)
+iimg = np.fft.ifft2(ishift)
+iimg = np.abs(iimg)
+
+# 显示原始图像
+plt.subplot(121),plt.imshow(img,cmap='gray')
+plt.title('original'),plt.axis('off')
+
+# 显示滤波结果
+plt.subplot(122),plt.imshow(iimg,cmap='gray')
+plt.title('iimg'),plt.axis('off')
+
+plt.savefig('../out/HFT_lena')
+plt.show()
